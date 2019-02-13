e1011a39c62fa3c5d840a770c4cc49dbad490f28
ad2969 <Clarenceadrian@gmail.com>
2019-02-13 10:55:16 -0800

    bugFixes

--- tampermonkey bot/tampermonkey-bot-v2.js
+++ tampermonkey bot/tampermonkey-bot-v2.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         WW ATTACK BOT
+// @name         WW BOT CHECKER
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  Simple bot for testing values by increments
+// @description  Simple bot for testing values by increments (uses check submission)
 // @author       You
 // @include      https://webwork.elearning.ubc.ca/*
 // @grant        none
@@ -16,17 +16,27 @@
     var webwork_mode = "single"; // **is there more than one webwork problem to solve?
     // webwork_modes: "single" - solve only one webwork question
     //                "multiple" - continue until all webwork questions solved
-    var solve_mode = "uniform"; // **is there more than one question to solve?
+    var solve_mode = "multiple"; // **is there more than one question to solve?
     // solve_modes: "single" - individual question solver
-    //              "multiple" - multiple question solver
-    //              "uniform" - solve multiple questions at the same time!
+    //              "sequential" - sequential question solver
+    //              "multiple" - solve multiple questions at the same time!
 
-    var start = 0; // **starting value
-    var end = 500; // **ending value
+    var start = 0.0; // **starting value
+    var end = 500.0; // **ending value
     var increment = 1; // **value to increment by
     var question_number = 1; // **which individual question to solve?
     var ignore_100 = 1; // **decide whether to ignore 100%
     var ignore_attempts = 1; // **decide whether to ignore unlimited attempts
+
+/************************** ADVANCED INCREMENT CODE ***************************/
+    var increment_mode = "simple"
+    // increment_modes: "simple" - normal incrementation
+    //                  "advanced" - use incrementation based on webwork percentage
+    var adv_start = 1;
+    var adv_end = 1000;
+    var adv_increment = 0;
+    var adv_webwork_error = 0.02; // 2% error on webwork
+    var dp = 8; // maximum decimal points to round to
 
 /************************** END OF CUSTOMIZABLE CODE **************************/
 
@@ -38,6 +48,7 @@
     var input_list = problem_body.getElementsByTagName("input");
     // get score summary (100%?)
     var score_summary = document.getElementById("score_summary");
+    var output_table = document.getElementsByClassName("attemptResults table table-condensed table-bordered")[0];
 
     // Button elements
     var submit_Button = document.getElementById("submitAnswers_id");
@@ -57,8 +68,8 @@
     }
 
     // input variables
-    var submitVal = 0;
-    var currentVal = 0;
+    var submitVal = 0.0;
+    var currentVal = 0.0;
     var counter = 0;
 
     // check webwork page
@@ -101,15 +112,19 @@
           else input_list[(question_number - 1)].setAttribute("checked","");
           submit_Button.click();
         }
-        // case: previous input exists
-        else if (currentVal < 0.0 | currentVal >= 0.0) {
+        // case: previous input exists (and is smaller than the end value)
+        else if ((currentVal < 0.0 | currentVal >= 0.0) && currentVal <= end) {
           submitVal = currentVal + increment;
+          if (submitVal > end) {
+            alert("Answer not found. Reached end of test values!");
+            back_Button.click();
+          }
           input_list[(question_number - 1) * 2].value = submitVal;
           submit_Button.click();
         }
-        // case: answer is undefined/null
+        // case: answer is undefined/null/larger than end value
         else {
-          submitVal = 0.0;
+          submitVal = start;
           input_list[(question_number - 1) * 2].value = submitVal;
           submit_Button.click();
         }
@@ -118,29 +133,31 @@
       else alert("Question is solved with value " + currentVal);
     }
 
+    // case: sequential solver
+    else if (solve_mode == "sequential") {
+      alert("sequential solver is really stupid actually");
+    }
+
     // case: multiple solver
     else if (solve_mode == "multiple") {
-      alert("multiple solver is really stupid actually");
-    }
-    
-    // case: uniform solver
-    else if (solve_mode == "uniform") {
       let check_flag = 0;
+      let unsolve_flag = 0;
       // for loop to fill in all questions
       for(counter = 1; counter <= parseInt(answers_list.length) / 3; counter++) {
         currentVal = parseFloat(eval(input_list[(counter - 1) * 2].value),10)
     // check if answer is correct
         // case: incorrect answer
         if (answers_list[counter * 3 - 1].classList.contains("ResultsWithError") == true) {
-    // check if answer input is currently null/undefined
-          // case: previous input exists
-          if (currentVal < 0.0 | currentVal >= 0.0) {
+    // check if answer input is currently null/undefined=
+          // case: previous input exists (and is lower than the end value)
+          if ((currentVal < 0.0 | currentVal >= 0.0) && currentVal <= end) {
             submitVal = currentVal + increment;
-            input_list[(counter - 1) * 2].value = submitVal;
+            if (submitVal > end) unsolve_flag++;
+            else input_list[(counter - 1) * 2].value = submitVal;
           }
           // case: answer is undefined/null
           else {
-            submitVal = 0.0;
+            submitVal = start;
             input_list[(counter - 1) * 2].value = submitVal;
           }
         }
@@ -153,6 +170,8 @@
         else if (webwork_mode == "multiple") alert("All problems solved");
         else alert("Question has been solved!");
       }
+      // if not all correct yet, and end values reached
+      else if(unsolve_flag * 2 == output_table.getElementsByClassName("ResultsWithError").length) alert("Question not fully solved. End value reached");
       // if not all correct yet
       else submit_Button.click(); // submit all filled answers
     }
